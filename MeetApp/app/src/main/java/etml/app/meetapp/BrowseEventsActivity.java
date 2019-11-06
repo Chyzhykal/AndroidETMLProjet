01ea3b94d4c54b2679f3db3a34ca02ccec2acd4e
booris <hutzlibo@etml.educanet2.ch>
2019-11-06 16:28:33 +0100

    Creating the views

---
+++ MeetApp/app/src/main/java/etml/app/meetapp/BrowseEventsActivity.java
@@ -0,0 +1,14 @@
+package etml.app.meetapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrowseEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_events);
+    }
+}
--- MeetApp/app/src/main/java/etml/app/meetapp/ProfileActivity.java
+++ MeetApp/app/src/main/java/etml/app/meetapp/ProfileActivity.java
@@ -1,13 +1,14 @@
 package etml.app.meetapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
     }
 }
