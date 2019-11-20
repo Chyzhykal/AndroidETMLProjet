3b7a450e6fd626f5375d96556d2cf3676be6dacd
Ouleg <chyzhykal@gmail.com>
2019-11-20 15:43:34 +0100

    Added database connection POC

--- MeetApp/app/src/main/java/etml/app/meetapp/database/MySQLConnect.java
+++ MeetApp/app/src/main/java/etml/app/meetapp/database/MySQLConnect.java
@@ -1,4 +1,4 @@
-/*package etml.app.meetapp.database;
+ package etml.app.meetapp.database;
 
 import android.os.AsyncTask;
 import android.widget.Toast;
@@ -9,51 +9,49 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
+import etml.app.meetapp.MainActivity;
 import etml.app.meetapp.R;
 
 
-class MySQLConnect extends AsyncTask<String, Void, String>{
-    private static final MySQLConnect ourInstance = new MySQLConnect();
+class MySQLConnect extends AsyncTask<String, Void, String> {
+    String res = "";
+    private  final String url = "jdbc:mysql://10.228.156.173:3306/db_meetapp";
+    private  final String user = "Buris";
+    private  final String pass = "root";
+    private Connection connection;
 
-    static MySQLConnect getInstance() {
-        return ourInstance;
-    }
-
-    private MySQLConnect() {
-
-    }
 
     @Override
-    protected void onPreExecute() {
+    public void onPreExecute() {
         super.onPreExecute();
-        Toast.makeText(MainActivity.this, "Please wait...", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
-    protected String doInBackground(String... params) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(Integer.toString(R.string.mysql_ip), Integer.toString(R.string.mysql_login), Integer.toString(R.string.mysql_pwd));
-            System.out.println("Databaseection success");
+    public String doInBackground(String... params) {
+     try {
+         Class.forName("com.mysql.jdbc.Driver");
+         Connection con = DriverManager.getConnection(url, user, pass);
+         System.out.println("Database connection success");
 
-            String result = "Database Connection Successful\n";
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery("select distinct Country from tblCountries");
-            ResultSetMetaData rsmd = rs.getMetaData();
+         String result = "Database Connection Successful\n";
+         Statement st = con.createStatement();
+         ResultSet rs = st.executeQuery("select catName from t_evCategory");
+         ResultSetMetaData rsmd = rs.getMetaData();
 
-            while (rs.next()) {
-                result += rs.getString(1).toString() + "\n";
-            }
-            res = result;
-        } catch (Exception e) {
-            e.printStackTrace();
-            res = e.toString();
-        }
-        return res;
+         while (rs.next()) {
+             result += rs.getString(1).toString() + "\n";
+         }
+         res = result;
+     } catch (Exception e) {
+         e.printStackTrace();
+         res = e.toString();
+     }
+     return res;
     }
 
     @Override
-    protected void onPostExecute(String result) {
-        txtData.setText(result);
+    public void onPostExecute(String result) {
+
     }
-}*/
+}
